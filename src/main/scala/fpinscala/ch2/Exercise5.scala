4a9193f06157019e63286ce17bb6d89d1c53da2d
Gianluca Carucci <rucka@tiscalinet.it>
2016-01-15 15:29:57 +0100

    Create Exercise5.scala

---
+++ src/main/scala/fpinscala/ch2/Exercise5.scala
@@ -0,0 +1,15 @@
+object Exercise5 {
+    def compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
+}
+
+import Exercise5._
+
+/*
+from repl you can test typing: 
+    :load src/main/scala/fpinscala/ch2/Exercise5.scala
+    val inc = (a: Int) => a + 1
+    val sqr = (a: Int) => a * a
+      
+    sqr(inc(2))
+    compose(sqr, inc)(2)
+*/
