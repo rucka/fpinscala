bcd7aba4b024c6e7d9fa6726506dfe1fd39fd345
rucka <rucka@tiscalinet.it>
2016-10-09 17:09:58 +0200

    ch3 ex22

---
+++ src/main/scala/fpinscala/ch3/Exercise22.scala
@@ -0,0 +1,20 @@
+package ch3
+import scala.annotation.tailrec
+import List._
+
+object Exercise22 {
+  def addPairwise(a: List[Int], b: List[Int]): List[Int] = (a, b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addPairwise(t1, t2))
+  }
+}
+
+import Exercise22._
+
+/*
+from repl you can test typing:
+  :load src/main/scala/fpinscala/ch3/List.scala
+  :load src/main/scala/fpinscala/ch3/Exercise22.scala
+  addPairwise(List(4,5,6), List(5,7,9))
+*/
