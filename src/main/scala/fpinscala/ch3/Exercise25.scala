5dcce09aa99f9b55801fb842cc498e5aff17dcd6
rucka <rucka@tiscalinet.it>
2016-10-09 18:40:01 +0200

    ch3 ex25

---
+++ src/main/scala/fpinscala/ch3/Exercise25.scala
@@ -0,0 +1,17 @@
+package ch3
+import scala.annotation.tailrec
+
+object Exercise25 {
+  def size[A](tree: Tree[A]): Int = tree match {
+    case Branch(l, r) => 1 + size(l) + size(r)
+    case Leaf(_) => 1
+  }
+}
+
+import Exercise25._
+
+/*
+from repl you can test typing:
+  :load src/main/scala/fpinscala/ch3/Tree.scala
+  :load src/main/scala/fpinscala/ch3/Exercise25.scala
+*/
