33e1d0c1075dd678465e2a3d49b7234c36f4c3dd
rucka <rucka@tiscalinet.it>
2016-10-12 08:23:27 +0200

    ch5 ex1

---
+++ src/main/scala/fpinscala/ch5/Exercise1.scala
@@ -0,0 +1,28 @@
+package ch5
+import scala.annotation.tailrec
+import Stream._
+
+object Exercise1 {
+  implicit class StreamExt[+A](val self: Stream[A]) extends AnyVal {
+    def toList: List[A] = self match {
+      case Empty => List.empty
+      case Cons(h, t) => h() :: t().toList
+    }
+
+    def toListTailRec: List[A] = {
+      @tailrec def loop(s: Stream[A], acc: List[A]): List[A] = s match {
+        case Empty => acc
+        case Cons(h, t) => loop(t(), h() :: acc)
+      }
+      loop(self, List.empty).reverse
+    }
+  }
+}
+
+import Exercise1._
+
+/*
+from repl you can test typing:
+  :load src/main/scala/fpinscala/ch5/Stream.scala
+  :load src/main/scala/fpinscala/ch5/Exercise1.scala
+*/
---
+++ src/main/scala/fpinscala/ch5/Stream.scala
@@ -0,0 +1,22 @@
+package ch5
+import Stream._
+trait Stream[+A] {
+
+}
+
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) Empty
+    else cons(as.head, apply(as.tail: _*))
+}
