1ef16c47633f9c5d597757a5118df74ebe908182
rucka <rucka@tiscalinet.it>
2016-12-21 08:42:03 +0100

    ch10 ex1

---
+++ src/main/scala/fpinscala/ch10/Exercise1.scala
@@ -0,0 +1,25 @@
+object ch10_1 {
+  import ch10._
+  val intAddition = new Monoid[Int] {
+    def op(a1: Int, a2: Int): Int = a1 + a2
+    def zero: Int = 0
+  }
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    def op(a1: Int, a2: Int): Int = a1 * a2
+    def zero: Int = 1
+  }
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+    def zero: Boolean = false
+  }
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+    def zero: Boolean = true
+  }
+}
+import ch10_1._
+/*
+from repl you can test typing:
+  :load src/main/scala/fpinscala/ch10/Monoid.scala
+  :load src/main/scala/fpinscala/ch10/Exercise1.scala
+*/
