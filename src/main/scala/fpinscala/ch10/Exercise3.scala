b4d652d672ab65fcc51cbb084ccb47404212724f
rucka <rucka@tiscalinet.it>
2016-12-21 08:54:18 +0100

    ch10 ex3

---
+++ src/main/scala/fpinscala/ch10/Exercise3.scala
@@ -0,0 +1,13 @@
+object ch10_3 {
+  import ch10._
+  def endoMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
+    def op(f: A => A, g: A => A): A => A = f compose g
+    def zero: A => A = a => a
+  }
+}
+import ch10_3._
+/*
+from repl you can test typing:
+  :load src/main/scala/fpinscala/ch10/Monoid.scala
+  :load src/main/scala/fpinscala/ch10/Exercise3.scala
+*/
