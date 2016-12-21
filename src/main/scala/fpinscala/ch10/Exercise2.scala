181dbf2f1e446e25488f3d775b1bfa963bfe248a
rucka <rucka@tiscalinet.it>
2016-12-21 08:49:52 +0100

    ch10 ex2

---
+++ src/main/scala/fpinscala/ch10/Exercise2.scala
@@ -0,0 +1,13 @@
+object ch10_2 {
+  import ch10._
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    def op(a1: Option[A], a2: Option[A]): Option[A] = a1 orElse a2
+    def zero: Option[A] = None
+  }
+}
+import ch10_2._
+/*
+from repl you can test typing:
+  :load src/main/scala/fpinscala/ch10/Monoid.scala
+  :load src/main/scala/fpinscala/ch10/Exercise2.scala
+*/
