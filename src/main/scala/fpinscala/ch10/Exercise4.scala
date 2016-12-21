e497cc7bb9a6552dcade78f0176c8f672a8f7a39
rucka <rucka@tiscalinet.it>
2016-12-21 17:14:19 +0100

    ch10 ex4 refactored

--- src/main/scala/fpinscala/ch10/Exercise4.scala
+++ src/main/scala/fpinscala/ch10/Exercise4.scala
@@ -1,20 +1,6 @@
 object ch10_4 {
   import ch10._
-  trait Prop {
-    def &&(b: Prop) : Prop
-  }
-  trait Gen[A] {
-    def flatMap[B](f: A => Gen[B]): Gen[B] = ???
-    def map[B](f: A => B): Gen[B] = ???
-  }
-  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = ???
-
-  def three[A](x: Gen[A])  = for {
-    a <- x
-    b <- x
-    c <- x
-  } yield (a, b, c)
-
+  import ch10.FakeScalaCheck._
   def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop = {
     val associativity: Prop = forAll(three(gen))(x =>
       m.op(x._1, m.op(x._2, x._3)) == m.op(m.op(x._1, x._2), x._3)
--- src/main/scala/fpinscala/ch10/Monoid.scala
+++ src/main/scala/fpinscala/ch10/Monoid.scala
@@ -3,4 +3,19 @@ object ch10 {
     def op(a1: A, a2: A): A
     def zero: A
   }
+  object FakeScalaCheck {
+    trait Prop {
+      def &&(b: Prop) : Prop
+    }
+    trait Gen[A] {
+      def flatMap[B](f: A => Gen[B]): Gen[B] = ???
+      def map[B](f: A => B): Gen[B] = ???
+    }
+    def forAll[A](as: Gen[A])(f: A => Boolean): Prop = ???
+    def three[A](x: Gen[A])  = for {
+      a <- x
+      b <- x
+      c <- x
+    } yield (a, b, c)
+  }
 }
