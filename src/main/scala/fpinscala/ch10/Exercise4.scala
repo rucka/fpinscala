c7c56d8959a185256cbc4c2e781c1b12aa2f3073
rucka <rucka@tiscalinet.it>
2016-12-22 17:07:06 +0100

    changed Prop used in ch10 ex4 to lib Gen

--- src/main/scala/fpinscala/ch10/Exercise4.scala
+++ src/main/scala/fpinscala/ch10/Exercise4.scala
@@ -1,6 +1,14 @@
 object ch10_4 {
   import ch10._
-  import ch10.FakeScalaCheck._
+  import lib_gen._
+  import Prop._
+
+  def three[A](x: Gen[A])  = for {
+    a <- x
+    b <- x
+    c <- x
+  } yield (a, b, c)
+
   def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop = {
     val associativity: Prop = forAll(three(gen))(x =>
       m.op(x._1, m.op(x._2, x._3)) == m.op(m.op(x._1, x._2), x._3)
@@ -12,6 +20,10 @@ object ch10_4 {
 import ch10_4._
 /*
 from repl you can test typing:
-  :load src/main/scala/fpinscala/ch10/Monoid.scala
-  :load src/main/scala/fpinscala/ch10/Exercise4.scala
+:load src/main/scala/fpinscala/lib/Par.Scala
+:load src/main/scala/fpinscala/lib/Stream.Scala
+:load src/main/scala/fpinscala/lib/State.Scala
+:load src/main/scala/fpinscala/lib/Gen.Scala
+:load src/main/scala/fpinscala/ch10/Monoid.scala
+:load src/main/scala/fpinscala/ch10/Exercise4.scala
 */
--- src/main/scala/fpinscala/ch10/Monoid.scala
+++ src/main/scala/fpinscala/ch10/Monoid.scala
@@ -39,19 +39,4 @@ object ch10 {
       m.op(foldMapV(pairs._1, m)(f), foldMapV(pairs._1, m)(f))
     }
   }
-  object FakeScalaCheck {
-    trait Prop {
-      def &&(b: Prop) : Prop
-    }
-    trait Gen[A] {
-      def flatMap[B](f: A => Gen[B]): Gen[B] = ???
-      def map[B](f: A => B): Gen[B] = ???
-    }
-    def forAll[A](as: Gen[A])(f: A => Boolean): Prop = ???
-    def three[A](x: Gen[A])  = for {
-      a <- x
-      b <- x
-      c <- x
-    } yield (a, b, c)
-  }
 }
