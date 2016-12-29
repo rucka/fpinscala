4c726cf24d375a1e2b631c9b84e52ca00eccf2e0
rucka <rucka@tiscalinet.it>
2016-12-29 09:11:42 +0100

    ch10 ex 12

---
+++ src/main/scala/fpinscala/ch10/Exercise12.scala
@@ -0,0 +1,44 @@
+object ch10_12 {
+  import ch10.Monoid
+  import scala.language.higherKinds
+
+
+  trait Foldable[F[_]] {
+    import Monoid._
+    def foldRight[A,B](as: F[A])(z: B)(f: (A,B) => B): B =
+      foldMap(as)(f.curried)(endoMonoid[B])(z)
+    def foldLeft[A,B](as: F[A])(z: B)(f: (B,A) => B): B =
+      foldMap(as)(a => (b: B) => f(b, a))(dual(endoMonoid[B]))(z)
+    def foldMap[A,B](as: F[A])(f: A => B)(mb: Monoid[B]): B =
+      foldRight(as)(mb.zero)((a, b) => mb.op(f(a), b))
+    def concatenate[A](as: F[A])(m: Monoid[A]): A =
+      foldLeft(as)(m.zero)(m.op)
+  }
+
+  object FoldableList extends Foldable[List] {
+    override def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B) =
+      as.foldRight(z)(f)
+    override def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B) =
+      as.foldLeft(z)(f)
+  }
+  object FoldableIndexedSeq extends Foldable[IndexedSeq] {
+    override def foldRight[A, B](as: IndexedSeq[A])(z: B)(f: (A, B) => B) =
+      as.foldRight(z)(f)
+    override def foldLeft[A, B](as: IndexedSeq[A])(z: B)(f: (B, A) => B) =
+      as.foldLeft(z)(f)
+    override def foldMap[A, B](as: IndexedSeq[A])(f: A => B)(mb: Monoid[B]): B =
+      Monoid.foldMapV(as, mb)(f)
+  }
+  object FoldableStream extends Foldable[Stream] {
+    override def foldRight[A, B](as: Stream[A])(z: B)(f: (A, B) => B) =
+      as.foldRight(z)((a, b) => f(a, b))
+    override def foldLeft[A, B](as: Stream[A])(z: B)(f: (B, A) => B) =
+      as.foldLeft(z)((b, a) => (f(b, a)))
+  }
+}
+import ch10_12._
+/*
+from repl you can test typing:
+:load src/main/scala/fpinscala/ch10/Monoid.scala
+:load src/main/scala/fpinscala/ch10/Exercise12.scala
+*/
--- src/main/scala/fpinscala/ch10/Monoid.scala
+++ src/main/scala/fpinscala/ch10/Monoid.scala
@@ -1,7 +1,19 @@
 object ch10 {
+  import scala.language.higherKinds
   trait Monoid[A] {
     def op(a1: A, a2: A): A
     def zero: A
+  }
+  trait Foldable[F[_]] {
+    import Monoid._
+    def foldRight[A,B](as: F[A])(z: B)(f: (A,B) => B): B =
+      foldMap(as)(f.curried)(endoMonoid[B])(z)
+    def foldLeft[A,B](as: F[A])(z: B)(f: (B,A) => B): B =
+      foldMap(as)(a => (b: B) => f(b, a))(dual(endoMonoid[B]))(z)
+    def foldMap[A,B](as: F[A])(f: A => B)(mb: Monoid[B]): B =
+      foldRight(as)(mb.zero)((a, b) => mb.op(f(a), b))
+    def concatenate[A](as: F[A])(m: Monoid[A]): A =
+      foldLeft(as)(m.zero)(m.op)
   }
   object Monoid {
     val intAddition = new Monoid[Int] {
