100199c256c709aae8374becdd2423891603372e
rucka <rucka@tiscalinet.it>
2017-01-11 19:03:05 +0100

    ch12 ex10 ex11 not done

--- src/main/scala/fpinscala/ch12/Exercise10.scala
+++ src/main/scala/fpinscala/ch12/Exercise10.scala
@@ -1,5 +1,8 @@
 object ch12_10 {
-  ???
+  //You will find this very difficult without an interactive proof assistant like Coq or Agda. If you do decide to take on the challenge, this is the kind of problem that might take someone several days or even a few weeks to think about.
+  // If `self` and `G` both satisfy the laws, then so does the composite.
+  // The full proof of the laws can be found at:
+  // https://github.com/runarorama/sannanir/blob/master/Applicative.v
 }
 import ch12_10._
 /*
---
+++ src/main/scala/fpinscala/ch12/Exercise11.scala
@@ -0,0 +1,8 @@
+object ch12_11 {
+  ???
+}
+import ch12_11._
+/*
+from repl you can test typing:
+:load src/main/scala/fpinscala/ch12/Exercise11.scala
+*/
