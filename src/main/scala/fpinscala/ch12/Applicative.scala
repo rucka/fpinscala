53cfa4009858093e965c8e7dce8c3a1fe53ff1f0
rucka <rucka@tiscalinet.it>
2017-01-11 09:40:09 +0100

    fixed applicative reference

--- src/main/scala/fpinscala/ch12/Applicative.scala
+++ src/main/scala/fpinscala/ch12/Applicative.scala
@@ -1,13 +1,23 @@
 object ch12 {
   import scala.language.higherKinds
+  import lib_monad.Functor
 
   trait Applicative[F[_]] extends Functor[F] {
-    def unit[A](a: A): F[A]
-    def apply[A,B](fab: F[A => B])(fa: F[A]): F[B]
+    def unit[A](a: => A): F[A]
+    def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+      apply(map(fa)(f.curried))(fb)
+    def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] =
+      map2(fab, fa)(_(_))
 
     def map[A,B](fa: F[A])(f: A => B): F[B] =
       apply(unit(f))(fa)
-    def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
-      apply(map(fa)(f.curried))(fb)
+    def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] =
+      as.foldRight(unit(List[B]()))((a, mbs) => map2(f(a), mbs)(_ :: _))
+    def sequence[A](fas: List[F[A]]): F[List[A]] =
+      traverse(fas)(identity)
+    def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
+      sequence(List.fill(n)(fa))
+    def product[A,B](fa: F[A], fb: F[B]): F[(A,B)] =
+      map2(fa, fb)((_,_))
   }
 }
